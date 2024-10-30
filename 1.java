338407db1f3671fee524d1c2d29454512dbc3581
tekim-design <tekim423@gmail.com>
2024-10-30 18:23:38 +0900

    version3

--- 1.java
+++ 1.java
@@ -3,10 +3,17 @@ public class HelloWorld {
 <<<<<<< HEAD
         System.out.println("이모 안녕asdfasdfdsafdsadasf");
 =======
-        System.out.println("이모 안녕");
-        System.out.println("이 줄은 삭제해야돼요.");
-        System.out.println("이모가 추가했어.");
-        
+        System.out.println("이모 안녕");        
+        System.out.println("이모가 추가했어11.");
+        System.out.println("이모가 추가했어22.");
+        System.out.println("이모가 추가했어33333.");
+        System.out.println("이모가 추가했어33333.");
+        System.out.println("이모가 추가했어33333.");
+        System.out.println("이모가 추가했어33333.");
+        지금여기 추가했는데 왜 커밋하게없다고나와!!!!
+        나 화가난다????
+        커밋하거라!git
+
 >>>>>>> 8f1148ffb260b86c59b29f08bd468404a690ceca
     }
 }
