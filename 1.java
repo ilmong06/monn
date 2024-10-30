bb3e15429088500b589692107cfffa38ec4e7c3c
ilmong06 <ilmongcoding@gmail.com>
2024-10-30 21:42:32 +0900

    version9

--- 1.java
+++ 1.java
@@ -1,28 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-        System.out.println("이모 안녕asdfasdfdsafdsadasf");
-=======
-        System.out.println("이모 안녕");        
-        System.out.println("이모가 추가했어11.");
-        System.out.println("이모가 추가했어22.");
-        System.out.println("이모가 추가했어33333.");
-        System.out.println("이모가 추가했어33333.");
-        System.out.println("이모가 추가했어33333.");
-        System.out.println("이모가 추가했어33333.");
-        지금여기 추가했는데 왜 커밋하게없다고나와!!!!
-        나 화가난다????dsd
-        커밋하거라!git
-
->>>>>>> 8f1148ffb260b86c59b29f08bd468404a690ceca
-=======
-        System.out.println("version6");
->>>>>>> 93caa15e9f8eadba664e7951ecb321bf1668e3d0
-=======
-        System.out.println("version8");
->>>>>>> aa
+        System.out.println("version9");
     }
 }
 
